aecd9ebf84a63a9b5ca12465a1254884dd4aead1
imran2975 <84298402+imran2975@users.noreply.github.com>
2022-06-13 21:45:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let count = 0
+
+function increment() {
+    document.querySelector("h2").innerText = count++;
+}
+
+function reset() {
+        document.querySelector("h2").innerText = 0;
+        count = 0;
+}
